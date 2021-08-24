7f969769a0ef97d3e9f358d718bf1a200244059a
ak-empereur-7 <41908463+ak-empereur-7@users.noreply.github.com>
2021-08-24 21:20:28 +0900

    use UrlParameter

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,11 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 export const UrlParameter = () => {
   const { id } = useParams();
+  /* useLocationというhooks使用することでクエリパラメータを使用できる
+     useLocationに標準であるsearchという変数に「?=hogehoge〜」の文字列が入っている
+  */
+  const { search } = useLocation();
+  /** JavaScriptの標準であるURLSearchParamsを使用することで「?=hogehoge〜」毎に取得できる*/
+  const query = new URLSearchParams(search);
   return (
     <div>
       <h1>UrlParameterページです</h1>
       <p>受け取ったパラメータは {id} です。</p>
+      <p>受け取ったクエリパラメータは {query.get("name")} です。</p>
     </div>
   );
 };
